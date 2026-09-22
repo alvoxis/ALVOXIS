df8fd0f44b46eb51e55b310c9de6c235ef4e1a67
alvoxis <hsrbn8gjfz@privaterelay.appleid.com>
2026-09-22 22:34:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -529,5 +529,15 @@ prevButton.addEventListener("click", function (event) {
   }
 
   updateCounter();
-  pageFlip.on("flip", updateCounter);
+  pageFlip.on("flip", function (event) {
+  const currentPage = event.data;
+
+  counter.textContent =
+    String(currentPage + 1).padStart(2, "0") +
+    " / " +
+    String(pages.length).padStart(2, "0");
+
+  prevButton.disabled = currentPage <= 0;
+  nextButton.disabled = currentPage >= pages.length - 1;
+});
 })();
